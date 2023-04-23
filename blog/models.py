849c1427f4543b5b9edf17248e6d474c2d9c36a3
newoldname <thjbjs@outlook.com>
2023-04-23 23:54:15 +0900

    image and file upload setting end

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.db import models
 
 
@@ -5,6 +7,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d", blank=True)
+    file_upload = models.FileField(upload_to="blog/files/%Y/%m/%d", blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -15,3 +20,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f"/blog/{self.pk}/"
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
