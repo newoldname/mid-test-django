5ed1f598e0063507b6985af4512ae8369f971fa9
newoldname <thjbjs@outlook.com>
2023-04-23 21:20:53 +0900

    change from FVB to CVB

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,10 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path("<int:pk>/", views.single_post_page),
-    path("", views.index),
+    # region FVB
+    # path("<int:pk>/", views.single_post_page),
+    # path("", views.index),
+    # endrregion
+    path("<int:pk>/", views.PostDetail.as_view()),
+    path("", views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,46 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
 
 # Create your views here.
-def index(request):
-    # posts = Post.objects.all()
-    posts = Post.objects.all().order_by("-pk")
 
-    return render(
-        request,
-        "blog/index.html",
-        {
-            "posts": posts,
-        },
-    )
+# region FVB
+# def index(request):
+#     # posts = Post.objects.all()
+#     posts = Post.objects.all().order_by("-pk")
+#
+#     return render(
+#         request,
+#         "blog/index.html",
+#         {
+#             "posts": posts,
+#         },
+#     )
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         "blog/single_post_page.html",
+#         {
+#             "post": post,
+#         },
+#     )
+# endregion
 
-    return render(
-        request,
-        "blog/single_post_page.html",
-        {
-            "post": post,
-        },
-    )
+
+class PostList(ListView):
+    model = Post
+    ordering = "-pk"
+    # 기본적으로 해당 클래스는 "{모델명}_list.html"을 사용하고.
+    # 랜더링할 변수 이름도 단순히 posts가 아닌 "{모델명}_list"이다.
+    # 만약에 템플릿 을 강제로 지정하고 싶으면
+    # template_name = 'blog/index.html'
+
+
+class PostDetail(DetailView):
+    model = Post
