f9398fc6a6e0874dd6727c43f84f96f781f9ec2e
newoldname <thjbjs@outlook.com>
2023-04-23 19:38:07 +0900

    add blog default url and formatting files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 # Create your views here.
+def index(request):
+    # posts = Post.objects.all()
+    posts = Post.objects.all().order_by("-pk")
+
+    return render(
+        request,
+        "blog/index.html",
+        {
+            "posts": posts,
+        },
+    )
