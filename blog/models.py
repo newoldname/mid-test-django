b3c58410312bfd4a9c8681b6f375fe735bc872cc
newoldname <thjbjs@outlook.com>
2023-04-23 17:19:58 +0900

    모델 / 테이블 추가 및 적용

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,9 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # 객체 안에서 원하는 함수를 작성할 수 있음
+    def __str__(self):
+        return f"[{self.pk}]{self.title}"
