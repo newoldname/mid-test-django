6aee6d1e97a706a44e3043b809625c4a50832305
newoldname <thjbjs@outlook.com>
2023-04-25 01:58:57 +0900

    use Post object at another app->single_pages

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 # Create your views here.
 def about_me(request):
@@ -11,8 +13,11 @@ def about_me(request):
 
 
 def landing(request):
+    posts = Post.objects.order_by("-pk")[:3]
     return render(
         request,
         "single_pages/landing.html",
-        {},
+        {
+            "posts": posts,
+        },
     )
